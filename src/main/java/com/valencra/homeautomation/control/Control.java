29f8449088b378b1be6018921194cffe1ccd835c
valencra <valencra@mcmaster.ca>
2018-02-11 23:08:30 -0500

    Added control entity

--- src/main/java/com/valencra/homeautomation/control/Control.java
+++ src/main/java/com/valencra/homeautomation/control/Control.java
@@ -1,4 +1,63 @@
 package com.valencra.homeautomation.control;
 
-public class Control {
+import com.valencra.homeautomation.core.BaseEntity;
+import com.valencra.homeautomation.device.Device;
+import com.valencra.homeautomation.user.User;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Control extends BaseEntity {
+  @NotNull
+  private String name;
+  @ManyToOne
+  private Device device;
+  @NotNull
+  private int value;
+  @ManyToOne
+  private User lastModifiedBy;
+
+  protected Control() {
+    super();
+  }
+
+  public Control(String name, int value) {
+    this();
+    this.name = name;
+    this.value = value;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Device getDevice() {
+    return device;
+  }
+
+  public void setDevice(Device device) {
+    this.device = device;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public void setValue(int value) {
+    this.value = value;
+  }
+
+  public User getLastModifiedBy() {
+    return lastModifiedBy;
+  }
+
+  public void setLastModifiedBy(User lastModifiedBy) {
+    this.lastModifiedBy = lastModifiedBy;
+  }
 }
