e50b579a84648bd4a58632138b2993013a4b6e0a
valencra <valencra@mcmaster.ca>
2018-02-12 22:35:52 -0500

    Devices now searcheable by partial name

--- src/main/java/com/valencra/homeautomation/device/DeviceRepository.java
+++ src/main/java/com/valencra/homeautomation/device/DeviceRepository.java
@@ -1,6 +1,13 @@
 package com.valencra.homeautomation.device;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface DeviceRepository extends PagingAndSortingRepository<Device, Long> {
+
+  @RestResource(rel = "name-contains", path = "nameContains")
+  Page<Device> findByNameContaining(@Param("name") String name, Pageable page);
 }
